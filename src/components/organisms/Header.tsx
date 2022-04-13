631adb918ca18045fd0c3117d595c26a6862fd36
mimosa23 <dev.00001001@gmail.com>
2022-04-13 21:30:40 +0900

    ヘッダーメニュー修正

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,26 +1,64 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  IconButton,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
-import BooksSearchForm from './BooksSearchForm';
+import CustomIcon from '../atoms/CustomIcon';
 
-const Header: FC = () => (
-  <Flex
-    as="nav"
-    align="center"
-    justify="space-between"
-    padding={6}
-    bg="blue.400"
-    shadow="md"
-  >
-    <Flex align="center" mr={6}>
-      <Link to="/">
-        <Heading as="h1" size="lg" letterSpacing="tighter">
-          Book management
-        </Heading>
-      </Link>
-    </Flex>
-    <BooksSearchForm />
-  </Flex>
-);
+const Header: FC = () => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <>
+      <Box>
+        <Flex
+          bg="white"
+          color="gray.600"
+          minH="60px"
+          py={{ base: 2 }}
+          px={{ base: 4 }}
+          borderBottom={1}
+          borderStyle="solid"
+          borderColor="gray.200"
+          align="center"
+        >
+          <Flex
+            flex={{ base: 1, md: 'auto' }}
+            ml={{ base: -2 }}
+            display={{ base: 'flex', md: 'none' }}
+          >
+            <IconButton
+              onClick={onToggle}
+              icon={
+                isOpen ? (
+                  <CustomIcon w={3} h={3} icon={AiOutlineClose} />
+                ) : (
+                  <CustomIcon w={5} h={5} icon={AiOutlineMenu} />
+                )
+              }
+              variant="ghost"
+              aria-label="Toggle Navigation"
+            />
+          </Flex>
+          <Flex align="center" mr={6}>
+            <Link to="/">
+              <Flex>
+                <Heading as="h1" size="lg" letterSpacing="tighter">
+                  reviews-library
+                </Heading>
+              </Flex>
+            </Link>
+          </Flex>
+        </Flex>
+        <Box flexGrow={3}>hogehoge</Box>
+      </Box>
+    </>
+  );
+};
 
 export default Header;
